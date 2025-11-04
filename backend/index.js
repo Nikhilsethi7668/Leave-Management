8f98fe567bb190ab11297ce125b721f7e022fed4
Nikhil Sethi <132539777+Nikhilsethi7668@users.noreply.github.com>
2025-11-04 12:59:59 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5001;
 // Middlewares
 app.use(
   cors({
-    origin: ["http://localhost:5174", "http://localhost:5173"],
+    origin: ["http://localhost:5174", "http://localhost:5173", "https://leave-management-kgeo.onrender.com"],
     credentials: true,
   })
 );
